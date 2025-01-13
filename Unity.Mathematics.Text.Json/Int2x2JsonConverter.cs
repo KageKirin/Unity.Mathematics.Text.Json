396d7ae3dd96fd3ddcd82e3578d350c37a4ad3c6
Christian Helmich <kagekirin@gmail.com>
2025-01-13 22:17:30 +0900

    feat: implement JsonConverter class for int2x2

---
+++ Unity.Mathematics.Text.Json/Int2x2JsonConverter.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Int2x2JsonConverter : JsonConverter<int2x2>
+{
+    public override int2x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new int2x2();
+
+        reader.Read();
+        value.c0.x = reader.GetInt32();
+
+        reader.Read();
+        value.c0.y = reader.GetInt32();
+
+        reader.Read();
+        value.c1.x = reader.GetInt32();
+
+        reader.Read();
+        value.c1.y = reader.GetInt32();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, int2x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.c0.x);
+
+        writer.WriteNumberValue(value.c0.y);
+
+        writer.WriteNumberValue(value.c1.x);
+
+        writer.WriteNumberValue(value.c1.y);
+
+        writer.WriteEndArray();
+    }
+}
