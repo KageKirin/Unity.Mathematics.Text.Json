af82af4eb7d2fa85b2b824093645f10ce36ba1fc
Christian Helmich <kagekirin@gmail.com>
2025-01-13 22:23:54 +0900

    feat: implement JsonConverter classes for Unity.Mathematics types (feat/implement-json-serializers) [#4]
    - **feat: implement JsonConverter class for float2**
    - **feat: implement JsonConverter class for float3**
    - **feat: implement JsonConverter class for float4**
    - **feat: implement JsonConverter class for quaternion**
    - **feat: implement JsonConverter class for float2x2**
    - **feat: implement JsonConverter class for float2x3**
    - **feat: implement JsonConverter class for float2x4**
    - **feat: implement JsonConverter class for float3x2**
    - **feat: implement JsonConverter class for float3x3**
    - **feat: implement JsonConverter class for float3x4**
    - **feat: implement JsonConverter class for float4x2**
    - **feat: implement JsonConverter class for float4x3**
    - **feat: implement JsonConverter class for float4x4**
    - **feat: implement JsonConverter class for int2**
    - **feat: implement JsonConverter class for int3**
    - **feat: implement JsonConverter class for int4**
    - **feat: implement JsonConverter class for int2x2**
    - **feat: implement JsonConverter class for int2x3**
    - **feat: implement JsonConverter class for int2x4**
    - **feat: implement JsonConverter class for int3x2**
    - **feat: implement JsonConverter class for int3x3**
    - **feat: implement JsonConverter class for int3x4**
    - **feat: implement JsonConverter class for int4x2**
    - **feat: implement JsonConverter class for int4x3**
    - **feat: implement JsonConverter class for int4x4**
    - **feat: implement JsonConverter class for uint2**
    - **feat: implement JsonConverter class for uint3**
    - **feat: implement JsonConverter class for uint4**
    - **feat: implement JsonConverter class for uint2x2**
    - **feat: implement JsonConverter class for uint2x3**
    - **feat: implement JsonConverter class for uint2x4**
    - **feat: implement JsonConverter class for uint3x2**
    - **feat: implement JsonConverter class for uint3x3**
    - **feat: implement JsonConverter class for uint3x4**
    - **feat: implement JsonConverter class for uint4x2**
    - **feat: implement JsonConverter class for uint4x3**
    - **feat: implement JsonConverter class for uint4x4**
    - **feat: implement JsonConverter class for double2**
    - **feat: implement JsonConverter class for double3**
    - **feat: implement JsonConverter class for double4**
    - **feat: implement JsonConverter class for double2x2**
    - **feat: implement JsonConverter class for double2x3**
    - **feat: implement JsonConverter class for double2x4**
    - **feat: implement JsonConverter class for double3x2**
    - **feat: implement JsonConverter class for double3x3**
    - **feat: implement JsonConverter class for double3x4**
    - **feat: implement JsonConverter class for double4x2**
    - **feat: implement JsonConverter class for double4x3**
    - **feat: implement JsonConverter class for double4x4**
    - **feat: implement JsonConverter class for bool2**
    - **feat: implement JsonConverter class for bool3**
    - **feat: implement JsonConverter class for bool4**
    - **feat: implement JsonConverter class for bool2x2**
    - **feat: implement JsonConverter class for bool2x3**
    - **feat: implement JsonConverter class for bool2x4**
    - **feat: implement JsonConverter class for bool3x2**
    - **feat: implement JsonConverter class for bool3x3**
    - **feat: implement JsonConverter class for bool3x4**
    - **feat: implement JsonConverter class for bool4x2**
    - **feat: implement JsonConverter class for bool4x3**
    - **feat: implement JsonConverter class for bool4x4**
    
    NOTE: hardcoded version generated from Python script using Liquid template (not included here).

---
+++ Unity.Mathematics.Text.Json/Bool4x4JsonConverter.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Bool4x4JsonConverter : JsonConverter<bool4x4>
+{
+    public override bool4x4 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new bool4x4();
+
+        reader.Read();
+        value.c0.x = reader.GetBoolean();
+
+        reader.Read();
+        value.c0.y = reader.GetBoolean();
+
+        reader.Read();
+        value.c0.z = reader.GetBoolean();
+
+        reader.Read();
+        value.c0.w = reader.GetBoolean();
+
+        reader.Read();
+        value.c1.x = reader.GetBoolean();
+
+        reader.Read();
+        value.c1.y = reader.GetBoolean();
+
+        reader.Read();
+        value.c1.z = reader.GetBoolean();
+
+        reader.Read();
+        value.c1.w = reader.GetBoolean();
+
+        reader.Read();
+        value.c2.x = reader.GetBoolean();
+
+        reader.Read();
+        value.c2.y = reader.GetBoolean();
+
+        reader.Read();
+        value.c2.z = reader.GetBoolean();
+
+        reader.Read();
+        value.c2.w = reader.GetBoolean();
+
+        reader.Read();
+        value.c3.x = reader.GetBoolean();
+
+        reader.Read();
+        value.c3.y = reader.GetBoolean();
+
+        reader.Read();
+        value.c3.z = reader.GetBoolean();
+
+        reader.Read();
+        value.c3.w = reader.GetBoolean();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool4x4 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteBooleanValue(value.c0.x);
+
+        writer.WriteBooleanValue(value.c0.y);
+
+        writer.WriteBooleanValue(value.c0.z);
+
+        writer.WriteBooleanValue(value.c0.w);
+
+        writer.WriteBooleanValue(value.c1.x);
+
+        writer.WriteBooleanValue(value.c1.y);
+
+        writer.WriteBooleanValue(value.c1.z);
+
+        writer.WriteBooleanValue(value.c1.w);
+
+        writer.WriteBooleanValue(value.c2.x);
+
+        writer.WriteBooleanValue(value.c2.y);
+
+        writer.WriteBooleanValue(value.c2.z);
+
+        writer.WriteBooleanValue(value.c2.w);
+
+        writer.WriteBooleanValue(value.c3.x);
+
+        writer.WriteBooleanValue(value.c3.y);
+
+        writer.WriteBooleanValue(value.c3.z);
+
+        writer.WriteBooleanValue(value.c3.w);
+
+        writer.WriteEndArray();
+    }
+}
---
+++ Unity.Mathematics.Text.Json/Float2x4JsonConverter.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Float2x4JsonConverter : JsonConverter<float2x4>
+{
+    public override float2x4 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new float2x4();
+
+        reader.Read();
+        value.c0.x = (float)reader.GetDouble();
+
+        reader.Read();
+        value.c0.y = (float)reader.GetDouble();
+
+        reader.Read();
+        value.c1.x = (float)reader.GetDouble();
+
+        reader.Read();
+        value.c1.y = (float)reader.GetDouble();
+
+        reader.Read();
+        value.c2.x = (float)reader.GetDouble();
+
+        reader.Read();
+        value.c2.y = (float)reader.GetDouble();
+
+        reader.Read();
+        value.c3.x = (float)reader.GetDouble();
+
+        reader.Read();
+        value.c3.y = (float)reader.GetDouble();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, float2x4 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.c0.x);
+
+        writer.WriteNumberValue(value.c0.y);
+
+        writer.WriteNumberValue(value.c1.x);
+
+        writer.WriteNumberValue(value.c1.y);
+
+        writer.WriteNumberValue(value.c2.x);
+
+        writer.WriteNumberValue(value.c2.y);
+
+        writer.WriteNumberValue(value.c3.x);
+
+        writer.WriteNumberValue(value.c3.y);
+
+        writer.WriteEndArray();
+    }
+}
