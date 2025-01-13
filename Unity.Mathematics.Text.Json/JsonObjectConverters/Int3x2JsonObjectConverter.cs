b50e8370d56383a5db826f50c5ee2ff6f44396e1
Christian Helmich <kagekirin@gmail.com>
2025-01-14 03:24:00 +0900

    feat: implement object-based JsonConverter class for int3x2

---
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Int3x2JsonObjectConverter.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Int3x2JsonObjectConverter : JsonConverter<int3x2>
+{
+    public override int3x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new int3x2();
+
+        reader.Read();
+        value.c0.x = reader.GetInt32("m00");
+
+        reader.Read();
+        value.c0.y = reader.GetInt32("m01");
+
+        reader.Read();
+        value.c0.z = reader.GetInt32("m02");
+
+        reader.Read();
+        value.c1.x = reader.GetInt32("m10");
+
+        reader.Read();
+        value.c1.y = reader.GetInt32("m11");
+
+        reader.Read();
+        value.c1.z = reader.GetInt32("m12");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, int3x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("m00", value.c0.x);
+
+        writer.WriteNumber("m01", value.c0.y);
+
+        writer.WriteNumber("m02", value.c0.z);
+
+        writer.WriteNumber("m10", value.c1.x);
+
+        writer.WriteNumber("m11", value.c1.y);
+
+        writer.WriteNumber("m12", value.c1.z);
+
+        writer.WriteEndObject();
+    }
+}
