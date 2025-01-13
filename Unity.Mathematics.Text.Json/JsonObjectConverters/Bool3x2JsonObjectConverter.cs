a4860cd6c1ed8597061ddc7de7208eabc7b446c0
Christian Helmich <kagekirin@gmail.com>
2025-01-14 03:25:08 +0900

    feat: implement object-based JsonConverter class for bool3x2

---
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Bool3x2JsonObjectConverter.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Bool3x2JsonObjectConverter : JsonConverter<bool3x2>
+{
+    public override bool3x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new bool3x2();
+
+        reader.Read();
+        value.c0.x = reader.GetBoolean("m00");
+
+        reader.Read();
+        value.c0.y = reader.GetBoolean("m01");
+
+        reader.Read();
+        value.c0.z = reader.GetBoolean("m02");
+
+        reader.Read();
+        value.c1.x = reader.GetBoolean("m10");
+
+        reader.Read();
+        value.c1.y = reader.GetBoolean("m11");
+
+        reader.Read();
+        value.c1.z = reader.GetBoolean("m12");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool3x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteBoolean("m00", value.c0.x);
+
+        writer.WriteBoolean("m01", value.c0.y);
+
+        writer.WriteBoolean("m02", value.c0.z);
+
+        writer.WriteBoolean("m10", value.c1.x);
+
+        writer.WriteBoolean("m11", value.c1.y);
+
+        writer.WriteBoolean("m12", value.c1.z);
+
+        writer.WriteEndObject();
+    }
+}
