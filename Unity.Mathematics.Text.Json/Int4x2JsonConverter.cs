498c1bed530cb60d1bde3129d4e0925d89605889
Christian Helmich <kagekirin@gmail.com>
2025-01-13 22:17:38 +0900

    feat: implement JsonConverter class for int4x2

---
+++ Unity.Mathematics.Text.Json/Int4x2JsonConverter.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Int4x2JsonConverter : JsonConverter<int4x2>
+{
+    public override int4x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new int4x2();
+
+        reader.Read();
+        value.c0.x = reader.GetInt32();
+
+        reader.Read();
+        value.c0.y = reader.GetInt32();
+
+        reader.Read();
+        value.c0.z = reader.GetInt32();
+
+        reader.Read();
+        value.c0.w = reader.GetInt32();
+
+        reader.Read();
+        value.c1.x = reader.GetInt32();
+
+        reader.Read();
+        value.c1.y = reader.GetInt32();
+
+        reader.Read();
+        value.c1.z = reader.GetInt32();
+
+        reader.Read();
+        value.c1.w = reader.GetInt32();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, int4x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.c0.x);
+
+        writer.WriteNumberValue(value.c0.y);
+
+        writer.WriteNumberValue(value.c0.z);
+
+        writer.WriteNumberValue(value.c0.w);
+
+        writer.WriteNumberValue(value.c1.x);
+
+        writer.WriteNumberValue(value.c1.y);
+
+        writer.WriteNumberValue(value.c1.z);
+
+        writer.WriteNumberValue(value.c1.w);
+
+        writer.WriteEndArray();
+    }
+}
