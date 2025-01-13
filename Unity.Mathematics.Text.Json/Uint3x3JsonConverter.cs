f47a2e6c570c5af8fd43b610f991c99b2d9bfa21
Christian Helmich <kagekirin@gmail.com>
2025-01-13 22:23:54 +0900

    feat: implement JsonConverter classes for Unity.Mathematics types (feat/implement-json-serializers) [#4]
    - **feat: implement JsonConverter class for float2**
    - **feat: implement JsonConverter class for float3**
    - **feat: implement JsonConverter class for float4**
    - **feat: implement JsonConverter class for quaternion**
    - **feat: implement JsonConverter class for float2x2**
    - **feat: implement JsonConverter class for float2x3**
    - **feat: implement JsonConverter class for float2x4**
    - **feat: implement JsonConverter class for float3x2**
    - **feat: implement JsonConverter class for float3x3**
    - **feat: implement JsonConverter class for float3x4**
    - **feat: implement JsonConverter class for float4x2**
    - **feat: implement JsonConverter class for float4x3**
    - **feat: implement JsonConverter class for float4x4**
    - **feat: implement JsonConverter class for int2**
    - **feat: implement JsonConverter class for int3**
    - **feat: implement JsonConverter class for int4**
    - **feat: implement JsonConverter class for int2x2**
    - **feat: implement JsonConverter class for int2x3**
    - **feat: implement JsonConverter class for int2x4**
    - **feat: implement JsonConverter class for int3x2**
    - **feat: implement JsonConverter class for int3x3**
    - **feat: implement JsonConverter class for int3x4**
    - **feat: implement JsonConverter class for int4x2**
    - **feat: implement JsonConverter class for int4x3**
    - **feat: implement JsonConverter class for int4x4**
    - **feat: implement JsonConverter class for uint2**
    - **feat: implement JsonConverter class for uint3**
    - **feat: implement JsonConverter class for uint4**
    - **feat: implement JsonConverter class for uint2x2**
    - **feat: implement JsonConverter class for uint2x3**
    - **feat: implement JsonConverter class for uint2x4**
    - **feat: implement JsonConverter class for uint3x2**
    - **feat: implement JsonConverter class for uint3x3**
    - **feat: implement JsonConverter class for uint3x4**
    - **feat: implement JsonConverter class for uint4x2**
    - **feat: implement JsonConverter class for uint4x3**
    - **feat: implement JsonConverter class for uint4x4**
    - **feat: implement JsonConverter class for double2**
    - **feat: implement JsonConverter class for double3**
    - **feat: implement JsonConverter class for double4**
    - **feat: implement JsonConverter class for double2x2**
    - **feat: implement JsonConverter class for double2x3**
    - **feat: implement JsonConverter class for double2x4**
    - **feat: implement JsonConverter class for double3x2**
    - **feat: implement JsonConverter class for double3x3**
    - **feat: implement JsonConverter class for double3x4**
    - **feat: implement JsonConverter class for double4x2**
    - **feat: implement JsonConverter class for double4x3**
    - **feat: implement JsonConverter class for double4x4**
    - **feat: implement JsonConverter class for bool2**
    - **feat: implement JsonConverter class for bool3**
    - **feat: implement JsonConverter class for bool4**
    - **feat: implement JsonConverter class for bool2x2**
    - **feat: implement JsonConverter class for bool2x3**
    - **feat: implement JsonConverter class for bool2x4**
    - **feat: implement JsonConverter class for bool3x2**
    - **feat: implement JsonConverter class for bool3x3**
    - **feat: implement JsonConverter class for bool3x4**
    - **feat: implement JsonConverter class for bool4x2**
    - **feat: implement JsonConverter class for bool4x3**
    - **feat: implement JsonConverter class for bool4x4**
    
    NOTE: hardcoded version generated from Python script using Liquid template (not included here).

---
+++ Unity.Mathematics.Text.Json/Uint3x3JsonConverter.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Uint3x3JsonConverter : JsonConverter<uint3x3>
+{
+    public override uint3x3 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new uint3x3();
+
+        reader.Read();
+        value.c0.x = reader.GetUInt32();
+
+        reader.Read();
+        value.c0.y = reader.GetUInt32();
+
+        reader.Read();
+        value.c0.z = reader.GetUInt32();
+
+        reader.Read();
+        value.c1.x = reader.GetUInt32();
+
+        reader.Read();
+        value.c1.y = reader.GetUInt32();
+
+        reader.Read();
+        value.c1.z = reader.GetUInt32();
+
+        reader.Read();
+        value.c2.x = reader.GetUInt32();
+
+        reader.Read();
+        value.c2.y = reader.GetUInt32();
+
+        reader.Read();
+        value.c2.z = reader.GetUInt32();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, uint3x3 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.c0.x);
+
+        writer.WriteNumberValue(value.c0.y);
+
+        writer.WriteNumberValue(value.c0.z);
+
+        writer.WriteNumberValue(value.c1.x);
+
+        writer.WriteNumberValue(value.c1.y);
+
+        writer.WriteNumberValue(value.c1.z);
+
+        writer.WriteNumberValue(value.c2.x);
+
+        writer.WriteNumberValue(value.c2.y);
+
+        writer.WriteNumberValue(value.c2.z);
+
+        writer.WriteEndArray();
+    }
+}
---
+++ Unity.Mathematics.Text.Json/Uint3x4JsonConverter.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Uint3x4JsonConverter : JsonConverter<uint3x4>
+{
+    public override uint3x4 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new uint3x4();
+
+        reader.Read();
+        value.c0.x = reader.GetUInt32();
+
+        reader.Read();
+        value.c0.y = reader.GetUInt32();
+
+        reader.Read();
+        value.c0.z = reader.GetUInt32();
+
+        reader.Read();
+        value.c1.x = reader.GetUInt32();
+
+        reader.Read();
+        value.c1.y = reader.GetUInt32();
+
+        reader.Read();
+        value.c1.z = reader.GetUInt32();
+
+        reader.Read();
+        value.c2.x = reader.GetUInt32();
+
+        reader.Read();
+        value.c2.y = reader.GetUInt32();
+
+        reader.Read();
+        value.c2.z = reader.GetUInt32();
+
+        reader.Read();
+        value.c3.x = reader.GetUInt32();
+
+        reader.Read();
+        value.c3.y = reader.GetUInt32();
+
+        reader.Read();
+        value.c3.z = reader.GetUInt32();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, uint3x4 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.c0.x);
+
+        writer.WriteNumberValue(value.c0.y);
+
+        writer.WriteNumberValue(value.c0.z);
+
+        writer.WriteNumberValue(value.c1.x);
+
+        writer.WriteNumberValue(value.c1.y);
+
+        writer.WriteNumberValue(value.c1.z);
+
+        writer.WriteNumberValue(value.c2.x);
+
+        writer.WriteNumberValue(value.c2.y);
+
+        writer.WriteNumberValue(value.c2.z);
+
+        writer.WriteNumberValue(value.c3.x);
+
+        writer.WriteNumberValue(value.c3.y);
+
+        writer.WriteNumberValue(value.c3.z);
+
+        writer.WriteEndArray();
+    }
+}
