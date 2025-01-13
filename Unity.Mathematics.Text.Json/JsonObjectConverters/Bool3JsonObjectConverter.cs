bc82bc5cff060e5c69f59bac5748064b71fe83f0
Christian Helmich <kagekirin@gmail.com>
2025-01-14 03:24:58 +0900

    feat: implement object-based JsonConverter class for bool3

---
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Bool3JsonObjectConverter.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Bool3JsonObjectConverter : JsonConverter<bool3>
+{
+    public override bool3 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new bool3();
+
+        reader.Read();
+        value.x = reader.GetBoolean("x");
+
+        reader.Read();
+        value.y = reader.GetBoolean("y");
+
+        reader.Read();
+        value.z = reader.GetBoolean("z");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool3 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteBoolean("x", value.x);
+
+        writer.WriteBoolean("y", value.y);
+
+        writer.WriteBoolean("z", value.z);
+
+        writer.WriteEndObject();
+    }
+}
