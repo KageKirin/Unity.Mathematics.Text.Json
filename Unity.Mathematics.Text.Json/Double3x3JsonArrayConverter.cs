a82a84e93b3cf9789326b2d40c26377c675cfb22
Christian Helmich <kagekirin@gmail.com>
2025-01-14 00:13:56 +0900

    refactor: rename Double3x3JsonConverter to Double3x3JsonArrayConverter

--- Unity.Mathematics.Text.Json/Double3x3JsonArrayConverter.cs
+++ Unity.Mathematics.Text.Json/Double3x3JsonArrayConverter.cs
@@ -6,7 +6,7 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Double3x3JsonConverter : JsonConverter<double3x3>
+public class Double3x3JsonArrayConverter : JsonConverter<double3x3>
 {
     public override double3x3 Read(
         ref Utf8JsonReader reader,
