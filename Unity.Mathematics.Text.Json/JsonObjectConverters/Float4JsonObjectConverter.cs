4a895ecf5ae2432145710590eb033801a1d90c5e
Christian Helmich <kagekirin@gmail.com>
2025-01-14 03:23:27 +0900

    feat: implement object-based JsonConverter class for float4

---
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Float4JsonObjectConverter.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Float4JsonObjectConverter : JsonConverter<float4>
+{
+    public override float4 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new float4();
+
+        reader.Read();
+        value.x = (float)reader.GetDouble("x");
+
+        reader.Read();
+        value.y = (float)reader.GetDouble("y");
+
+        reader.Read();
+        value.z = (float)reader.GetDouble("z");
+
+        reader.Read();
+        value.w = (float)reader.GetDouble("w");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, float4 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("x", value.x);
+
+        writer.WriteNumber("y", value.y);
+
+        writer.WriteNumber("z", value.z);
+
+        writer.WriteNumber("w", value.w);
+
+        writer.WriteEndObject();
+    }
+}
