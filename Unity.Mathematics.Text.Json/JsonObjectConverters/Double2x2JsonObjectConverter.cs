e297804e5f2f45e93b06174bbde99f135b908887
Christian Helmich <kagekirin@gmail.com>
2025-01-14 03:24:39 +0900

    feat: implement object-based JsonConverter class for double2x2

---
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Double2x2JsonObjectConverter.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public class Double2x2JsonObjectConverter : JsonConverter<double2x2>
+{
+    public override double2x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new double2x2();
+
+        reader.Read();
+        value.c0.x = reader.GetDouble("m00");
+
+        reader.Read();
+        value.c0.y = reader.GetDouble("m01");
+
+        reader.Read();
+        value.c1.x = reader.GetDouble("m10");
+
+        reader.Read();
+        value.c1.y = reader.GetDouble("m11");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        double2x2 value,
+        JsonSerializerOptions options
+    )
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("m00", value.c0.x);
+
+        writer.WriteNumber("m01", value.c0.y);
+
+        writer.WriteNumber("m10", value.c1.x);
+
+        writer.WriteNumber("m11", value.c1.y);
+
+        writer.WriteEndObject();
+    }
+}
