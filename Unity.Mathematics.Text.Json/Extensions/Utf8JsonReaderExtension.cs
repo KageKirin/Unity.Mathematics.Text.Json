29e1b20d9bc089b8e34939894f2ef515b0e7334b
Christian Helmich <kagekirin@gmail.com>
2025-01-14 11:19:33 +0900

    feat: implement Utf8JsonReader.GetUInt32(string propertyName) extension method

--- Unity.Mathematics.Text.Json/Extensions/Utf8JsonReaderExtension.cs
+++ Unity.Mathematics.Text.Json/Extensions/Utf8JsonReaderExtension.cs
@@ -48,4 +48,23 @@ public static class Utf8JsonReaderExtension
         return reader.GetInt32();
     }
 
+    public static uint GetUInt32(this ref Utf8JsonReader reader, string propertyName)
+    {
+        if (reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new JsonException(
+                $"Expected PropertyName token ('{propertyName}'), but got {reader.TokenType}"
+            );
+        }
+
+        var propName = reader.GetString();
+        if (propName != propertyName)
+        {
+            throw new JsonException($"Expected PropertyName token == '{propertyName}'");
+        }
+
+        reader.Read();
+        return reader.GetUInt32();
+    }
+
 }
