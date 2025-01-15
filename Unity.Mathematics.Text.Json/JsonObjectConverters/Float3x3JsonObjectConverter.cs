9340fa2b5a48984174f920d75a2845a2bff74225
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:14 +0900

    refactor: object-based JsonConverter for float3x3 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Float3x3JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Float3x3JsonObjectConverter.cs
@@ -6,79 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Float3x3JsonObjectConverter : JsonConverter<float3x3>
+public class Float3x3JsonObjectConverter : Float3x3JsonConverter
 {
-    public override float3x3 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new float3x3();
-
-        reader.Read();
-        value.c0.x = (float)reader.GetDouble("m00");
-
-        reader.Read();
-        value.c0.y = (float)reader.GetDouble("m01");
-
-        reader.Read();
-        value.c0.z = (float)reader.GetDouble("m02");
-
-        reader.Read();
-        value.c1.x = (float)reader.GetDouble("m10");
-
-        reader.Read();
-        value.c1.y = (float)reader.GetDouble("m11");
-
-        reader.Read();
-        value.c1.z = (float)reader.GetDouble("m12");
-
-        reader.Read();
-        value.c2.x = (float)reader.GetDouble("m20");
-
-        reader.Read();
-        value.c2.y = (float)reader.GetDouble("m21");
-
-        reader.Read();
-        value.c2.z = (float)reader.GetDouble("m22");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, float3x3 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteNumber("m00", value.c0.x);
-
-        writer.WriteNumber("m01", value.c0.y);
-
-        writer.WriteNumber("m02", value.c0.z);
-
-        writer.WriteNumber("m10", value.c1.x);
-
-        writer.WriteNumber("m11", value.c1.y);
-
-        writer.WriteNumber("m12", value.c1.z);
-
-        writer.WriteNumber("m20", value.c2.x);
-
-        writer.WriteNumber("m21", value.c2.y);
-
-        writer.WriteNumber("m22", value.c2.z);
-
-        writer.WriteEndObject();
-    }
+    public Float3x3JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
