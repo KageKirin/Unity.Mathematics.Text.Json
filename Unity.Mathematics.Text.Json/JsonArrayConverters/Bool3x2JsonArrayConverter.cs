3e0bd48b6b515246470ebd86f18137e053170122
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:13 +0900

    refactor: array-based JsonConverter for bool3x2 inherits from common class

--- Unity.Mathematics.Text.Json/JsonArrayConverters/Bool3x2JsonArrayConverter.cs
+++ Unity.Mathematics.Text.Json/JsonArrayConverters/Bool3x2JsonArrayConverter.cs
@@ -6,64 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Bool3x2JsonArrayConverter : JsonConverter<bool3x2>
+public class Bool3x2JsonArrayConverter : Bool3x2JsonConverter
 {
-    public override bool3x2 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartArray)
-        {
-            throw new JsonException();
-        }
-
-        var value = new bool3x2();
-
-        reader.Read();
-        value.c0.x = reader.GetBoolean();
-
-        reader.Read();
-        value.c0.y = reader.GetBoolean();
-
-        reader.Read();
-        value.c0.z = reader.GetBoolean();
-
-        reader.Read();
-        value.c1.x = reader.GetBoolean();
-
-        reader.Read();
-        value.c1.y = reader.GetBoolean();
-
-        reader.Read();
-        value.c1.z = reader.GetBoolean();
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndArray)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, bool3x2 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartArray();
-
-        writer.WriteBooleanValue(value.c0.x);
-
-        writer.WriteBooleanValue(value.c0.y);
-
-        writer.WriteBooleanValue(value.c0.z);
-
-        writer.WriteBooleanValue(value.c1.x);
-
-        writer.WriteBooleanValue(value.c1.y);
-
-        writer.WriteBooleanValue(value.c1.z);
-
-        writer.WriteEndArray();
-    }
+    public Bool3x2JsonArrayConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartArray //!< write as array
+        ) { }
 }
