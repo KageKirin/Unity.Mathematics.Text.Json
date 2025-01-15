741479d05317bcc9e87d78eaef93d1ac753f259a
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:16 +0900

    refactor: object-based JsonConverter for double2 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Double2JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Double2JsonObjectConverter.cs
@@ -6,44 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Double2JsonObjectConverter : JsonConverter<double2>
+public class Double2JsonObjectConverter : Double2JsonConverter
 {
-    public override double2 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new double2();
-
-        reader.Read();
-        value.x = reader.GetDouble("x");
-
-        reader.Read();
-        value.y = reader.GetDouble("y");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, double2 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteNumber("x", value.x);
-
-        writer.WriteNumber("y", value.y);
-
-        writer.WriteEndObject();
-    }
+    public Double2JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
