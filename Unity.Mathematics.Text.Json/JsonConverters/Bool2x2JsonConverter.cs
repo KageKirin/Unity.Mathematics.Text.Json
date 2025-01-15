aff56776e207728948dc191f838d9c02e89908f3
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:29:07 +0900

    feat: implement common JsonConverter class for bool2x2

--- Unity.Mathematics.Text.Json/JsonConverters/Bool2x2JsonConverter.cs
+++ Unity.Mathematics.Text.Json/JsonConverters/Bool2x2JsonConverter.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -7,24 +8,15 @@ using Unity.Mathematics;
 namespace Unity.Mathematics.Text.Json;
 
 public abstract class Bool2x2JsonConverter : JsonConverter<bool2x2>
+
 {
-    public delegate bool2x2 ReadFunc(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    );
 
-    public delegate void WriteFunc(
-        Utf8JsonWriter writer,
-        bool2x2 value,
-        JsonSerializerOptions options
-    );
+    public delegate bool2x2 ReadFunc(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options);
 
-    public bool2x2 ReadAsArray(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
+    public delegate void WriteFunc(Utf8JsonWriter writer, bool2x2 value, JsonSerializerOptions options);
+
+    public bool2x2 ReadAsArray(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
     {
         if (reader.TokenType != JsonTokenType.StartArray)
         {
@@ -32,18 +24,19 @@ public abstract class Bool2x2JsonConverter : JsonConverter<bool2x2>
         }
 
         var value = new bool2x2();
-
+        
         reader.Read();
         value.c0.x = reader.GetBoolean();
-
+        
         reader.Read();
         value.c0.y = reader.GetBoolean();
-
+        
         reader.Read();
         value.c1.x = reader.GetBoolean();
-
+        
         reader.Read();
         value.c1.y = reader.GetBoolean();
+        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndArray)
@@ -55,25 +48,26 @@ public abstract class Bool2x2JsonConverter : JsonConverter<bool2x2>
     }
 
     public void WriteAsArray(Utf8JsonWriter writer, bool2x2 value, JsonSerializerOptions options)
+
     {
+
         writer.WriteStartArray();
-
+        
         writer.WriteBooleanValue(value.c0.x);
-
+        
         writer.WriteBooleanValue(value.c0.y);
-
+        
         writer.WriteBooleanValue(value.c1.x);
-
+        
         writer.WriteBooleanValue(value.c1.y);
-
+        
         writer.WriteEndArray();
+
     }
 
-    public bool2x2 ReadAsObject(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
+
+    public bool2x2 ReadAsObject(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
     {
         if (reader.TokenType != JsonTokenType.StartObject)
         {
@@ -81,18 +75,19 @@ public abstract class Bool2x2JsonConverter : JsonConverter<bool2x2>
         }
 
         var value = new bool2x2();
-
+        
         reader.Read();
         value.c0.x = reader.GetBoolean("m00");
-
+        
         reader.Read();
         value.c0.y = reader.GetBoolean("m01");
-
+        
         reader.Read();
         value.c1.x = reader.GetBoolean("m10");
-
+        
         reader.Read();
         value.c1.y = reader.GetBoolean("m11");
+        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndObject)
@@ -104,65 +99,61 @@ public abstract class Bool2x2JsonConverter : JsonConverter<bool2x2>
     }
 
     public void WriteAsObject(Utf8JsonWriter writer, bool2x2 value, JsonSerializerOptions options)
+
     {
+
         writer.WriteStartObject();
-
+        
         writer.WriteBoolean("m00", value.c0.x);
-
+        
         writer.WriteBoolean("m01", value.c0.y);
-
+        
         writer.WriteBoolean("m10", value.c1.x);
-
+        
         writer.WriteBoolean("m11", value.c1.y);
-
+        
         writer.WriteEndObject();
+
     }
 
-    public bool2x2 ReadCompatible(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    ) =>
-        reader.TokenType switch
+    public bool2x2 ReadCompatible(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
+    =>
+         reader.TokenType switch
         {
             JsonTokenType.StartArray => ReadAsArray(ref reader, typeToConvert, options),
             JsonTokenType.StartObject => ReadAsObject(ref reader, typeToConvert, options),
             _ => throw new JsonException(),
         };
+    
 
     private readonly ReadFunc readFunc;
     private readonly WriteFunc writeFunc;
 
-    public Bool2x2JsonConverter(
-        JsonTokenType readerTokenType = JsonTokenType.None,
-        JsonTokenType writerTokenType = JsonTokenType.None
-    )
-        : base()
+    public Bool2x2JsonConverter(JsonTokenType readerTokenType = JsonTokenType.None, JsonTokenType writerTokenType = JsonTokenType.None) : base()
+
     {
-        readFunc = readerTokenType switch
-        {
+
+        readFunc = readerTokenType switch {
             JsonTokenType.StartArray => ReadAsArray,
             JsonTokenType.StartObject => ReadAsObject,
             _ => ReadCompatible,
         };
 
-        writeFunc = writerTokenType switch
-        {
+        writeFunc = writerTokenType switch {
             JsonTokenType.StartArray => WriteAsArray,
             JsonTokenType.StartObject => WriteAsObject,
             _ => WriteAsArray, //!< we need _some_ kind of default
         };
+
     }
 
-    public override bool2x2 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    ) => readFunc(ref reader, typeToConvert, options);
 
-    public override void Write(
-        Utf8JsonWriter writer,
-        bool2x2 value,
-        JsonSerializerOptions options
-    ) => writeFunc(writer, value, options);
+    public override bool2x2 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => readFunc(ref reader, typeToConvert, options);
+
+    public override void Write(Utf8JsonWriter writer, bool2x2 value, JsonSerializerOptions options)
+        => writeFunc(writer, value, options);
+
 }
+
