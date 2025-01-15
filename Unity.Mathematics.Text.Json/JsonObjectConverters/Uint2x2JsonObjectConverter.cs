73b047d4b189c16493ba52b91128c973f6f16677
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:15 +0900

    refactor: object-based JsonConverter for uint2x2 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Uint2x2JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Uint2x2JsonObjectConverter.cs
@@ -6,54 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Uint2x2JsonObjectConverter : JsonConverter<uint2x2>
+public class Uint2x2JsonObjectConverter : Uint2x2JsonConverter
 {
-    public override uint2x2 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new uint2x2();
-
-        reader.Read();
-        value.c0.x = reader.GetUInt32("m00");
-
-        reader.Read();
-        value.c0.y = reader.GetUInt32("m01");
-
-        reader.Read();
-        value.c1.x = reader.GetUInt32("m10");
-
-        reader.Read();
-        value.c1.y = reader.GetUInt32("m11");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, uint2x2 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteNumber("m00", value.c0.x);
-
-        writer.WriteNumber("m01", value.c0.y);
-
-        writer.WriteNumber("m10", value.c1.x);
-
-        writer.WriteNumber("m11", value.c1.y);
-
-        writer.WriteEndObject();
-    }
+    public Uint2x2JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
