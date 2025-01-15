25d019b4e22b05c9808623a8f5cfadd1ef7070b5
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:15 +0900

    refactor: object-based JsonConverter for int4x3 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Int4x3JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Int4x3JsonObjectConverter.cs
@@ -6,94 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Int4x3JsonObjectConverter : JsonConverter<int4x3>
+public class Int4x3JsonObjectConverter : Int4x3JsonConverter
 {
-    public override int4x3 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new int4x3();
-
-        reader.Read();
-        value.c0.x = reader.GetInt32("m00");
-
-        reader.Read();
-        value.c0.y = reader.GetInt32("m01");
-
-        reader.Read();
-        value.c0.z = reader.GetInt32("m02");
-
-        reader.Read();
-        value.c0.w = reader.GetInt32("m03");
-
-        reader.Read();
-        value.c1.x = reader.GetInt32("m10");
-
-        reader.Read();
-        value.c1.y = reader.GetInt32("m11");
-
-        reader.Read();
-        value.c1.z = reader.GetInt32("m12");
-
-        reader.Read();
-        value.c1.w = reader.GetInt32("m13");
-
-        reader.Read();
-        value.c2.x = reader.GetInt32("m20");
-
-        reader.Read();
-        value.c2.y = reader.GetInt32("m21");
-
-        reader.Read();
-        value.c2.z = reader.GetInt32("m22");
-
-        reader.Read();
-        value.c2.w = reader.GetInt32("m23");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, int4x3 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteNumber("m00", value.c0.x);
-
-        writer.WriteNumber("m01", value.c0.y);
-
-        writer.WriteNumber("m02", value.c0.z);
-
-        writer.WriteNumber("m03", value.c0.w);
-
-        writer.WriteNumber("m10", value.c1.x);
-
-        writer.WriteNumber("m11", value.c1.y);
-
-        writer.WriteNumber("m12", value.c1.z);
-
-        writer.WriteNumber("m13", value.c1.w);
-
-        writer.WriteNumber("m20", value.c2.x);
-
-        writer.WriteNumber("m21", value.c2.y);
-
-        writer.WriteNumber("m22", value.c2.z);
-
-        writer.WriteNumber("m23", value.c2.w);
-
-        writer.WriteEndObject();
-    }
+    public Int4x3JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
