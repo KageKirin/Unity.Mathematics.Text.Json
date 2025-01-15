6d60aaf71cd7ad9942f9845ffc3ec0020b781e88
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:12 +0900

    refactor: array-based JsonConverter for uint4x3 inherits from common class

--- Unity.Mathematics.Text.Json/JsonArrayConverters/Uint4x3JsonArrayConverter.cs
+++ Unity.Mathematics.Text.Json/JsonArrayConverters/Uint4x3JsonArrayConverter.cs
@@ -6,94 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Uint4x3JsonArrayConverter : JsonConverter<uint4x3>
+public class Uint4x3JsonArrayConverter : Uint4x3JsonConverter
 {
-    public override uint4x3 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartArray)
-        {
-            throw new JsonException();
-        }
-
-        var value = new uint4x3();
-
-        reader.Read();
-        value.c0.x = reader.GetUInt32();
-
-        reader.Read();
-        value.c0.y = reader.GetUInt32();
-
-        reader.Read();
-        value.c0.z = reader.GetUInt32();
-
-        reader.Read();
-        value.c0.w = reader.GetUInt32();
-
-        reader.Read();
-        value.c1.x = reader.GetUInt32();
-
-        reader.Read();
-        value.c1.y = reader.GetUInt32();
-
-        reader.Read();
-        value.c1.z = reader.GetUInt32();
-
-        reader.Read();
-        value.c1.w = reader.GetUInt32();
-
-        reader.Read();
-        value.c2.x = reader.GetUInt32();
-
-        reader.Read();
-        value.c2.y = reader.GetUInt32();
-
-        reader.Read();
-        value.c2.z = reader.GetUInt32();
-
-        reader.Read();
-        value.c2.w = reader.GetUInt32();
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndArray)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, uint4x3 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartArray();
-
-        writer.WriteNumberValue(value.c0.x);
-
-        writer.WriteNumberValue(value.c0.y);
-
-        writer.WriteNumberValue(value.c0.z);
-
-        writer.WriteNumberValue(value.c0.w);
-
-        writer.WriteNumberValue(value.c1.x);
-
-        writer.WriteNumberValue(value.c1.y);
-
-        writer.WriteNumberValue(value.c1.z);
-
-        writer.WriteNumberValue(value.c1.w);
-
-        writer.WriteNumberValue(value.c2.x);
-
-        writer.WriteNumberValue(value.c2.y);
-
-        writer.WriteNumberValue(value.c2.z);
-
-        writer.WriteNumberValue(value.c2.w);
-
-        writer.WriteEndArray();
-    }
+    public Uint4x3JsonArrayConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartArray //!< write as array
+        ) { }
 }
