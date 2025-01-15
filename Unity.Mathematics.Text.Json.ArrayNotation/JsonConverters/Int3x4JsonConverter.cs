ea8bd1f9bb2ec2dfd614f86e6ec2efef59871cf6
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:05 +0900

    feat: implement array notation-only JsonConverter class for int3x4

---
+++ Unity.Mathematics.Text.Json.ArrayNotation/JsonConverters/Int3x4JsonConverter.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ArrayNotation;
+
+public class Int3x4JsonConverter : JsonConverter<int3x4>
+{
+    public override int3x4 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new int3x4();
+
+        reader.Read();
+        value.c0.x = reader.GetInt32();
+
+        reader.Read();
+        value.c0.y = reader.GetInt32();
+
+        reader.Read();
+        value.c0.z = reader.GetInt32();
+
+        reader.Read();
+        value.c1.x = reader.GetInt32();
+
+        reader.Read();
+        value.c1.y = reader.GetInt32();
+
+        reader.Read();
+        value.c1.z = reader.GetInt32();
+
+        reader.Read();
+        value.c2.x = reader.GetInt32();
+
+        reader.Read();
+        value.c2.y = reader.GetInt32();
+
+        reader.Read();
+        value.c2.z = reader.GetInt32();
+
+        reader.Read();
+        value.c3.x = reader.GetInt32();
+
+        reader.Read();
+        value.c3.y = reader.GetInt32();
+
+        reader.Read();
+        value.c3.z = reader.GetInt32();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, int3x4 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.c0.x);
+
+        writer.WriteNumberValue(value.c0.y);
+
+        writer.WriteNumberValue(value.c0.z);
+
+        writer.WriteNumberValue(value.c1.x);
+
+        writer.WriteNumberValue(value.c1.y);
+
+        writer.WriteNumberValue(value.c1.z);
+
+        writer.WriteNumberValue(value.c2.x);
+
+        writer.WriteNumberValue(value.c2.y);
+
+        writer.WriteNumberValue(value.c2.z);
+
+        writer.WriteNumberValue(value.c3.x);
+
+        writer.WriteNumberValue(value.c3.y);
+
+        writer.WriteNumberValue(value.c3.z);
+
+        writer.WriteEndArray();
+    }
+}
