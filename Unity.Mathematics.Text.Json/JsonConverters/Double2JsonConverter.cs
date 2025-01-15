4526c67244abbd5c33945e46f89326aefbcbc70a
Christian Helmich <kagekirin@gmail.com>
2025-01-15 22:12:50 +0900

    refactor: change order of field and function declarations in JsonConverter class for double2

--- Unity.Mathematics.Text.Json/JsonConverters/Double2JsonConverter.cs
+++ Unity.Mathematics.Text.Json/JsonConverters/Double2JsonConverter.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -7,16 +6,60 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public abstract class Double2JsonConverter : JsonConverter<double2>
-
+public class Double2JsonConverter : JsonConverter<double2>
 {
+    public delegate double2 ReadFunc(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    );
+    public delegate void WriteFunc(
+        Utf8JsonWriter writer,
+        double2 value,
+        JsonSerializerOptions options
+    );
 
-    public delegate double2 ReadFunc(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options);
+    private readonly ReadFunc readFunc;
+    private readonly WriteFunc writeFunc;
 
-    public delegate void WriteFunc(Utf8JsonWriter writer, double2 value, JsonSerializerOptions options);
+    public Double2JsonConverter(
+        JsonTokenType readerTokenType = JsonTokenType.None,
+        JsonTokenType writerTokenType = JsonTokenType.None
+    )
+        : base()
+    {
+        readFunc = readerTokenType switch
+        {
+            JsonTokenType.StartArray => ReadAsArray,
+            JsonTokenType.StartObject => ReadAsObject,
+            _ => ReadCompatible,
+        };
 
-    public double2 ReadAsArray(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        writeFunc = writerTokenType switch
+        {
+            JsonTokenType.StartArray => WriteAsArray,
+            JsonTokenType.StartObject => WriteAsObject,
+            _ => WriteAsArray, //!< we need _some_ kind of default
+        };
+    }
 
+    public override double2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    ) => readFunc(ref reader, typeToConvert, options);
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        double2 value,
+        JsonSerializerOptions options
+    ) => writeFunc(writer, value, options);
+
+    public double2 ReadAsArray(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
     {
         if (reader.TokenType != JsonTokenType.StartArray)
         {
@@ -24,13 +67,12 @@ public abstract class Double2JsonConverter : JsonConverter<double2>
         }
 
         var value = new double2();
-        
+
         reader.Read();
         value.x = reader.GetDouble();
-        
+
         reader.Read();
         value.y = reader.GetDouble();
-        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndArray)
@@ -42,22 +84,18 @@ public abstract class Double2JsonConverter : JsonConverter<double2>
     }
 
     public void WriteAsArray(Utf8JsonWriter writer, double2 value, JsonSerializerOptions options)
-
     {
-
         writer.WriteStartArray();
-        
         writer.WriteNumberValue(value.x);
-        
         writer.WriteNumberValue(value.y);
-        
         writer.WriteEndArray();
-
     }
 
-
-    public double2 ReadAsObject(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-
+    public double2 ReadAsObject(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
     {
         if (reader.TokenType != JsonTokenType.StartObject)
         {
@@ -65,13 +103,12 @@ public abstract class Double2JsonConverter : JsonConverter<double2>
         }
 
         var value = new double2();
-        
+
         reader.Read();
         value.x = reader.GetDouble("x");
-        
+
         reader.Read();
         value.y = reader.GetDouble("y");
-        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndObject)
@@ -83,57 +120,22 @@ public abstract class Double2JsonConverter : JsonConverter<double2>
     }
 
     public void WriteAsObject(Utf8JsonWriter writer, double2 value, JsonSerializerOptions options)
-
     {
-
         writer.WriteStartObject();
-        
         writer.WriteNumber("x", value.x);
-        
         writer.WriteNumber("y", value.y);
-        
         writer.WriteEndObject();
-
     }
 
-    public double2 ReadCompatible(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-
-    =>
-         reader.TokenType switch
+    public double2 ReadCompatible(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    ) =>
+        reader.TokenType switch
         {
             JsonTokenType.StartArray => ReadAsArray(ref reader, typeToConvert, options),
             JsonTokenType.StartObject => ReadAsObject(ref reader, typeToConvert, options),
             _ => throw new JsonException(),
         };
-    
-
-    private readonly ReadFunc readFunc;
-    private readonly WriteFunc writeFunc;
-
-    public Double2JsonConverter(JsonTokenType readerTokenType = JsonTokenType.None, JsonTokenType writerTokenType = JsonTokenType.None) : base()
-
-    {
-
-        readFunc = readerTokenType switch {
-            JsonTokenType.StartArray => ReadAsArray,
-            JsonTokenType.StartObject => ReadAsObject,
-            _ => ReadCompatible,
-        };
-
-        writeFunc = writerTokenType switch {
-            JsonTokenType.StartArray => WriteAsArray,
-            JsonTokenType.StartObject => WriteAsObject,
-            _ => WriteAsArray, //!< we need _some_ kind of default
-        };
-
-    }
-
-
-    public override double2 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        => readFunc(ref reader, typeToConvert, options);
-
-    public override void Write(Utf8JsonWriter writer, double2 value, JsonSerializerOptions options)
-        => writeFunc(writer, value, options);
-
 }
-
