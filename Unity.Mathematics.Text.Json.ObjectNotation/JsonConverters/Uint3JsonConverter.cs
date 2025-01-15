d63125807d0ecb15a059b426c8641704d298ba7e
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:09 +0900

    feat: implement object notation-only JsonConverter class for uint3

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/JsonConverters/Uint3JsonConverter.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public class Uint3JsonConverter : JsonConverter<uint3>
+{
+    public override uint3 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new uint3();
+
+        reader.Read();
+        value.x = reader.GetUInt32("x");
+
+        reader.Read();
+        value.y = reader.GetUInt32("y");
+
+        reader.Read();
+        value.z = reader.GetUInt32("z");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, uint3 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("x", value.x);
+
+        writer.WriteNumber("y", value.y);
+
+        writer.WriteNumber("z", value.z);
+
+        writer.WriteEndObject();
+    }
+}
