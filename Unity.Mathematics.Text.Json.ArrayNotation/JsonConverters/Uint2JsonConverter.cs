2fafcb02a6194cfc652e44281823cead38461f11
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:06 +0900

    feat: implement array notation-only JsonConverter class for uint2

---
+++ Unity.Mathematics.Text.Json.ArrayNotation/JsonConverters/Uint2JsonConverter.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ArrayNotation;
+
+public class Uint2JsonConverter : JsonConverter<uint2>
+{
+    public override uint2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new uint2();
+
+        reader.Read();
+        value.x = reader.GetUInt32();
+
+        reader.Read();
+        value.y = reader.GetUInt32();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, uint2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.x);
+
+        writer.WriteNumberValue(value.y);
+
+        writer.WriteEndArray();
+    }
+}
