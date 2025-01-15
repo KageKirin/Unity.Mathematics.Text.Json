b8a84d7f108ed1f1f3743cd366b33f94e800b2bb
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:16 +0900

    refactor: object-based JsonConverter for double4x3 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Double4x3JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Double4x3JsonObjectConverter.cs
@@ -6,98 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Double4x3JsonObjectConverter : JsonConverter<double4x3>
+public class Double4x3JsonObjectConverter : Double4x3JsonConverter
 {
-    public override double4x3 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new double4x3();
-
-        reader.Read();
-        value.c0.x = reader.GetDouble("m00");
-
-        reader.Read();
-        value.c0.y = reader.GetDouble("m01");
-
-        reader.Read();
-        value.c0.z = reader.GetDouble("m02");
-
-        reader.Read();
-        value.c0.w = reader.GetDouble("m03");
-
-        reader.Read();
-        value.c1.x = reader.GetDouble("m10");
-
-        reader.Read();
-        value.c1.y = reader.GetDouble("m11");
-
-        reader.Read();
-        value.c1.z = reader.GetDouble("m12");
-
-        reader.Read();
-        value.c1.w = reader.GetDouble("m13");
-
-        reader.Read();
-        value.c2.x = reader.GetDouble("m20");
-
-        reader.Read();
-        value.c2.y = reader.GetDouble("m21");
-
-        reader.Read();
-        value.c2.z = reader.GetDouble("m22");
-
-        reader.Read();
-        value.c2.w = reader.GetDouble("m23");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(
-        Utf8JsonWriter writer,
-        double4x3 value,
-        JsonSerializerOptions options
-    )
-    {
-        writer.WriteStartObject();
-
-        writer.WriteNumber("m00", value.c0.x);
-
-        writer.WriteNumber("m01", value.c0.y);
-
-        writer.WriteNumber("m02", value.c0.z);
-
-        writer.WriteNumber("m03", value.c0.w);
-
-        writer.WriteNumber("m10", value.c1.x);
-
-        writer.WriteNumber("m11", value.c1.y);
-
-        writer.WriteNumber("m12", value.c1.z);
-
-        writer.WriteNumber("m13", value.c1.w);
-
-        writer.WriteNumber("m20", value.c2.x);
-
-        writer.WriteNumber("m21", value.c2.y);
-
-        writer.WriteNumber("m22", value.c2.z);
-
-        writer.WriteNumber("m23", value.c2.w);
-
-        writer.WriteEndObject();
-    }
+    public Double4x3JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
