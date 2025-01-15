aa8fc97fccbfcb018f5c5d32dcc43ef53ed5d3b6
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:21:56 +0900

    feat: implement common JsonConverter class for double2x2

---
+++ Unity.Mathematics.Text.Json/JsonConverters/Double2x2JsonConverter.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json;
+
+public abstract class Double2x2JsonConverter : JsonConverter<double2x2>
+{
+    public delegate double2x2 ReadFunc(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    );
+
+    public delegate void WriteFunc(
+        Utf8JsonWriter writer,
+        double2x2 value,
+        JsonSerializerOptions options
+    );
+
+    public double2x2 ReadAsArray(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new double2x2();
+
+        reader.Read();
+        value.c0.x = reader.GetDouble();
+
+        reader.Read();
+        value.c0.y = reader.GetDouble();
+
+        reader.Read();
+        value.c1.x = reader.GetDouble();
+
+        reader.Read();
+        value.c1.y = reader.GetDouble();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public void WriteAsArray(Utf8JsonWriter writer, double2x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.c0.x);
+
+        writer.WriteNumberValue(value.c0.y);
+
+        writer.WriteNumberValue(value.c1.x);
+
+        writer.WriteNumberValue(value.c1.y);
+
+        writer.WriteEndArray();
+    }
+
+    public double2x2 ReadAsObject(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new double2x2();
+
+        reader.Read();
+        value.c0.x = reader.GetDouble("m00");
+
+        reader.Read();
+        value.c0.y = reader.GetDouble("m01");
+
+        reader.Read();
+        value.c1.x = reader.GetDouble("m10");
+
+        reader.Read();
+        value.c1.y = reader.GetDouble("m11");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public void WriteAsObject(Utf8JsonWriter writer, double2x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("m00", value.c0.x);
+
+        writer.WriteNumber("m01", value.c0.y);
+
+        writer.WriteNumber("m10", value.c1.x);
+
+        writer.WriteNumber("m11", value.c1.y);
+
+        writer.WriteEndObject();
+    }
+
+    public double2x2 ReadCompatible(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    ) =>
+        reader.TokenType switch
+        {
+            JsonTokenType.StartArray => ReadAsArray(ref reader, typeToConvert, options),
+            JsonTokenType.StartObject => ReadAsObject(ref reader, typeToConvert, options),
+            _ => throw new JsonException(),
+        };
+
+    private readonly ReadFunc readFunc;
+    private readonly WriteFunc writeFunc;
+
+    public Double2x2JsonConverter(
+        JsonTokenType readerTokenType = JsonTokenType.None,
+        JsonTokenType writerTokenType = JsonTokenType.None
+    )
+        : base()
+    {
+        readFunc = readerTokenType switch
+        {
+            JsonTokenType.StartArray => ReadAsArray,
+            JsonTokenType.StartObject => ReadAsObject,
+            _ => ReadCompatible,
+        };
+
+        writeFunc = writerTokenType switch
+        {
+            JsonTokenType.StartArray => WriteAsArray,
+            JsonTokenType.StartObject => WriteAsObject,
+            _ => WriteAsArray, //!< we need _some_ kind of default
+        };
+    }
+
+    public override double2x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    ) => readFunc(ref reader, typeToConvert, options);
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        double2x2 value,
+        JsonSerializerOptions options
+    ) => writeFunc(writer, value, options);
+}
