84cdddedf0a98f4372a0f0f02befd89e3f96d664
Christian Helmich <kagekirin@gmail.com>
2025-01-15 22:12:04 +0900

    refactor: change order of field and function declarations in JsonConverter class for uint3

--- Unity.Mathematics.Text.Json/JsonConverters/Uint3JsonConverter.cs
+++ Unity.Mathematics.Text.Json/JsonConverters/Uint3JsonConverter.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -7,16 +6,57 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public abstract class Uint3JsonConverter : JsonConverter<uint3>
-
+public class Uint3JsonConverter : JsonConverter<uint3>
 {
+    public delegate uint3 ReadFunc(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    );
+    public delegate void WriteFunc(
+        Utf8JsonWriter writer,
+        uint3 value,
+        JsonSerializerOptions options
+    );
 
-    public delegate uint3 ReadFunc(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options);
+    private readonly ReadFunc readFunc;
+    private readonly WriteFunc writeFunc;
 
-    public delegate void WriteFunc(Utf8JsonWriter writer, uint3 value, JsonSerializerOptions options);
+    public Uint3JsonConverter(
+        JsonTokenType readerTokenType = JsonTokenType.None,
+        JsonTokenType writerTokenType = JsonTokenType.None
+    )
+        : base()
+    {
+        readFunc = readerTokenType switch
+        {
+            JsonTokenType.StartArray => ReadAsArray,
+            JsonTokenType.StartObject => ReadAsObject,
+            _ => ReadCompatible,
+        };
 
-    public uint3 ReadAsArray(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        writeFunc = writerTokenType switch
+        {
+            JsonTokenType.StartArray => WriteAsArray,
+            JsonTokenType.StartObject => WriteAsObject,
+            _ => WriteAsArray, //!< we need _some_ kind of default
+        };
+    }
 
+    public override uint3 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    ) => readFunc(ref reader, typeToConvert, options);
+
+    public override void Write(Utf8JsonWriter writer, uint3 value, JsonSerializerOptions options) =>
+        writeFunc(writer, value, options);
+
+    public uint3 ReadAsArray(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
     {
         if (reader.TokenType != JsonTokenType.StartArray)
         {
@@ -24,16 +64,15 @@ public abstract class Uint3JsonConverter : JsonConverter<uint3>
         }
 
         var value = new uint3();
-        
+
         reader.Read();
         value.x = reader.GetUInt32();
-        
+
         reader.Read();
         value.y = reader.GetUInt32();
-        
+
         reader.Read();
         value.z = reader.GetUInt32();
-        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndArray)
@@ -45,24 +84,19 @@ public abstract class Uint3JsonConverter : JsonConverter<uint3>
     }
 
     public void WriteAsArray(Utf8JsonWriter writer, uint3 value, JsonSerializerOptions options)
-
     {
-
         writer.WriteStartArray();
-        
         writer.WriteNumberValue(value.x);
-        
         writer.WriteNumberValue(value.y);
-        
         writer.WriteNumberValue(value.z);
-        
         writer.WriteEndArray();
-
     }
 
-
-    public uint3 ReadAsObject(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-
+    public uint3 ReadAsObject(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
     {
         if (reader.TokenType != JsonTokenType.StartObject)
         {
@@ -70,16 +104,15 @@ public abstract class Uint3JsonConverter : JsonConverter<uint3>
         }
 
         var value = new uint3();
-        
+
         reader.Read();
         value.x = reader.GetUInt32("x");
-        
+
         reader.Read();
         value.y = reader.GetUInt32("y");
-        
+
         reader.Read();
         value.z = reader.GetUInt32("z");
-        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndObject)
@@ -91,59 +124,23 @@ public abstract class Uint3JsonConverter : JsonConverter<uint3>
     }
 
     public void WriteAsObject(Utf8JsonWriter writer, uint3 value, JsonSerializerOptions options)
-
     {
-
         writer.WriteStartObject();
-        
         writer.WriteNumber("x", value.x);
-        
         writer.WriteNumber("y", value.y);
-        
         writer.WriteNumber("z", value.z);
-        
         writer.WriteEndObject();
-
     }
 
-    public uint3 ReadCompatible(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-
-    =>
-         reader.TokenType switch
+    public uint3 ReadCompatible(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    ) =>
+        reader.TokenType switch
         {
             JsonTokenType.StartArray => ReadAsArray(ref reader, typeToConvert, options),
             JsonTokenType.StartObject => ReadAsObject(ref reader, typeToConvert, options),
             _ => throw new JsonException(),
         };
-    
-
-    private readonly ReadFunc readFunc;
-    private readonly WriteFunc writeFunc;
-
-    public Uint3JsonConverter(JsonTokenType readerTokenType = JsonTokenType.None, JsonTokenType writerTokenType = JsonTokenType.None) : base()
-
-    {
-
-        readFunc = readerTokenType switch {
-            JsonTokenType.StartArray => ReadAsArray,
-            JsonTokenType.StartObject => ReadAsObject,
-            _ => ReadCompatible,
-        };
-
-        writeFunc = writerTokenType switch {
-            JsonTokenType.StartArray => WriteAsArray,
-            JsonTokenType.StartObject => WriteAsObject,
-            _ => WriteAsArray, //!< we need _some_ kind of default
-        };
-
-    }
-
-
-    public override uint3 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        => readFunc(ref reader, typeToConvert, options);
-
-    public override void Write(Utf8JsonWriter writer, uint3 value, JsonSerializerOptions options)
-        => writeFunc(writer, value, options);
-
 }
-
