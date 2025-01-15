ecc2b12d6f0488d9f77e9c9ac4ea2e09f275da76
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:16 +0900

    refactor: object-based JsonConverter for double4 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Double4JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Double4JsonObjectConverter.cs
@@ -6,54 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Double4JsonObjectConverter : JsonConverter<double4>
+public class Double4JsonObjectConverter : Double4JsonConverter
 {
-    public override double4 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new double4();
-
-        reader.Read();
-        value.x = reader.GetDouble("x");
-
-        reader.Read();
-        value.y = reader.GetDouble("y");
-
-        reader.Read();
-        value.z = reader.GetDouble("z");
-
-        reader.Read();
-        value.w = reader.GetDouble("w");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, double4 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteNumber("x", value.x);
-
-        writer.WriteNumber("y", value.y);
-
-        writer.WriteNumber("z", value.z);
-
-        writer.WriteNumber("w", value.w);
-
-        writer.WriteEndObject();
-    }
+    public Double4JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
