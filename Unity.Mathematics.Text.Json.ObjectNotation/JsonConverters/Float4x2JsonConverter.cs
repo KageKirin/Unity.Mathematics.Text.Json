9cfe22c445a4123686a68d288724fcc6b34d5d4d
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:08 +0900

    feat: implement object notation-only JsonConverter class for float4x2

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/JsonConverters/Float4x2JsonConverter.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public class Float4x2JsonConverter : JsonConverter<float4x2>
+{
+    public override float4x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new float4x2();
+
+        reader.Read();
+        value.c0.x = (float)reader.GetDouble("m00");
+
+        reader.Read();
+        value.c0.y = (float)reader.GetDouble("m01");
+
+        reader.Read();
+        value.c0.z = (float)reader.GetDouble("m02");
+
+        reader.Read();
+        value.c0.w = (float)reader.GetDouble("m03");
+
+        reader.Read();
+        value.c1.x = (float)reader.GetDouble("m10");
+
+        reader.Read();
+        value.c1.y = (float)reader.GetDouble("m11");
+
+        reader.Read();
+        value.c1.z = (float)reader.GetDouble("m12");
+
+        reader.Read();
+        value.c1.w = (float)reader.GetDouble("m13");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, float4x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("m00", value.c0.x);
+
+        writer.WriteNumber("m01", value.c0.y);
+
+        writer.WriteNumber("m02", value.c0.z);
+
+        writer.WriteNumber("m03", value.c0.w);
+
+        writer.WriteNumber("m10", value.c1.x);
+
+        writer.WriteNumber("m11", value.c1.y);
+
+        writer.WriteNumber("m12", value.c1.z);
+
+        writer.WriteNumber("m13", value.c1.w);
+
+        writer.WriteEndObject();
+    }
+}
