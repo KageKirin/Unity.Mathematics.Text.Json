5ee8b286b9609dd9d06833805c2a16344ddf710d
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:10 +0900

    refactor: array-based JsonConverter for int3 inherits from common class

--- Unity.Mathematics.Text.Json/JsonArrayConverters/Int3JsonArrayConverter.cs
+++ Unity.Mathematics.Text.Json/JsonArrayConverters/Int3JsonArrayConverter.cs
@@ -6,49 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Int3JsonArrayConverter : JsonConverter<int3>
+public class Int3JsonArrayConverter : Int3JsonConverter
 {
-    public override int3 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartArray)
-        {
-            throw new JsonException();
-        }
-
-        var value = new int3();
-
-        reader.Read();
-        value.x = reader.GetInt32();
-
-        reader.Read();
-        value.y = reader.GetInt32();
-
-        reader.Read();
-        value.z = reader.GetInt32();
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndArray)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, int3 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartArray();
-
-        writer.WriteNumberValue(value.x);
-
-        writer.WriteNumberValue(value.y);
-
-        writer.WriteNumberValue(value.z);
-
-        writer.WriteEndArray();
-    }
+    public Int3JsonArrayConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartArray //!< write as array
+        ) { }
 }
