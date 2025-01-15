c48d74a0be1a26f27d20de842aea29a44cfa89d7
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:09 +0900

    feat: implement object notation-only JsonConverter class for int2

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/JsonConverters/Int2JsonConverter.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public class Int2JsonConverter : JsonConverter<int2>
+{
+    public override int2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new int2();
+
+        reader.Read();
+        value.x = reader.GetInt32("x");
+
+        reader.Read();
+        value.y = reader.GetInt32("y");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, int2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("x", value.x);
+
+        writer.WriteNumber("y", value.y);
+
+        writer.WriteEndObject();
+    }
+}
