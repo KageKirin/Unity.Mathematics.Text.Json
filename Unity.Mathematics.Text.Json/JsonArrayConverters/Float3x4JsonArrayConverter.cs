ca1b3997d25c524d99436042ec0f742e3dd65f58
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:10 +0900

    refactor: array-based JsonConverter for float3x4 inherits from common class

--- Unity.Mathematics.Text.Json/JsonArrayConverters/Float3x4JsonArrayConverter.cs
+++ Unity.Mathematics.Text.Json/JsonArrayConverters/Float3x4JsonArrayConverter.cs
@@ -6,94 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Float3x4JsonArrayConverter : JsonConverter<float3x4>
+public class Float3x4JsonArrayConverter : Float3x4JsonConverter
 {
-    public override float3x4 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartArray)
-        {
-            throw new JsonException();
-        }
-
-        var value = new float3x4();
-
-        reader.Read();
-        value.c0.x = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c0.y = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c0.z = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c1.x = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c1.y = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c1.z = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c2.x = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c2.y = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c2.z = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c3.x = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c3.y = (float)reader.GetDouble();
-
-        reader.Read();
-        value.c3.z = (float)reader.GetDouble();
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndArray)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, float3x4 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartArray();
-
-        writer.WriteNumberValue(value.c0.x);
-
-        writer.WriteNumberValue(value.c0.y);
-
-        writer.WriteNumberValue(value.c0.z);
-
-        writer.WriteNumberValue(value.c1.x);
-
-        writer.WriteNumberValue(value.c1.y);
-
-        writer.WriteNumberValue(value.c1.z);
-
-        writer.WriteNumberValue(value.c2.x);
-
-        writer.WriteNumberValue(value.c2.y);
-
-        writer.WriteNumberValue(value.c2.z);
-
-        writer.WriteNumberValue(value.c3.x);
-
-        writer.WriteNumberValue(value.c3.y);
-
-        writer.WriteNumberValue(value.c3.z);
-
-        writer.WriteEndArray();
-    }
+    public Float3x4JsonArrayConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartArray //!< write as array
+        ) { }
 }
