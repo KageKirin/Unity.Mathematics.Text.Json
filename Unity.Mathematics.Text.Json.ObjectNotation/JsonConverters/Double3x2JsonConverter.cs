6fd1707c37c58ebb1795169da9902e14bbade893
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:10 +0900

    feat: implement object notation-only JsonConverter class for double3x2

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/JsonConverters/Double3x2JsonConverter.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public class Double3x2JsonConverter : JsonConverter<double3x2>
+{
+    public override double3x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new double3x2();
+
+        reader.Read();
+        value.c0.x = reader.GetDouble("m00");
+
+        reader.Read();
+        value.c0.y = reader.GetDouble("m01");
+
+        reader.Read();
+        value.c0.z = reader.GetDouble("m02");
+
+        reader.Read();
+        value.c1.x = reader.GetDouble("m10");
+
+        reader.Read();
+        value.c1.y = reader.GetDouble("m11");
+
+        reader.Read();
+        value.c1.z = reader.GetDouble("m12");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        double3x2 value,
+        JsonSerializerOptions options
+    )
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("m00", value.c0.x);
+
+        writer.WriteNumber("m01", value.c0.y);
+
+        writer.WriteNumber("m02", value.c0.z);
+
+        writer.WriteNumber("m10", value.c1.x);
+
+        writer.WriteNumber("m11", value.c1.y);
+
+        writer.WriteNumber("m12", value.c1.z);
+
+        writer.WriteEndObject();
+    }
+}
