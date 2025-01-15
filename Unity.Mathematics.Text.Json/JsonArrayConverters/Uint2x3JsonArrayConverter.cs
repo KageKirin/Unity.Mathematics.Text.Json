53d72bb3fde594b83c037a03ecd43d49516137e1
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:11 +0900

    refactor: array-based JsonConverter for uint2x3 inherits from common class

--- Unity.Mathematics.Text.Json/JsonArrayConverters/Uint2x3JsonArrayConverter.cs
+++ Unity.Mathematics.Text.Json/JsonArrayConverters/Uint2x3JsonArrayConverter.cs
@@ -6,64 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Uint2x3JsonArrayConverter : JsonConverter<uint2x3>
+public class Uint2x3JsonArrayConverter : Uint2x3JsonConverter
 {
-    public override uint2x3 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartArray)
-        {
-            throw new JsonException();
-        }
-
-        var value = new uint2x3();
-
-        reader.Read();
-        value.c0.x = reader.GetUInt32();
-
-        reader.Read();
-        value.c0.y = reader.GetUInt32();
-
-        reader.Read();
-        value.c1.x = reader.GetUInt32();
-
-        reader.Read();
-        value.c1.y = reader.GetUInt32();
-
-        reader.Read();
-        value.c2.x = reader.GetUInt32();
-
-        reader.Read();
-        value.c2.y = reader.GetUInt32();
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndArray)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, uint2x3 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartArray();
-
-        writer.WriteNumberValue(value.c0.x);
-
-        writer.WriteNumberValue(value.c0.y);
-
-        writer.WriteNumberValue(value.c1.x);
-
-        writer.WriteNumberValue(value.c1.y);
-
-        writer.WriteNumberValue(value.c2.x);
-
-        writer.WriteNumberValue(value.c2.y);
-
-        writer.WriteEndArray();
-    }
+    public Uint2x3JsonArrayConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartArray //!< write as array
+        ) { }
 }
