1edaa5e59a1278739dde15634c15370cdc49004b
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:04:53 +0900

    feat: add copy of Utf8JsonReader extension methods to ObjectNotation project

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/Utf8JsonReaderExtension.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+#nullable enable
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public static class Utf8JsonReaderExtension
+{
+    public static double GetDouble(this ref Utf8JsonReader reader, string propertyName)
+    {
+        if (reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new JsonException(
+                $"Expected PropertyName token ('{propertyName}'), but got {reader.TokenType}"
+            );
+        }
+
+        var propName = reader.GetString();
+        if (propName != propertyName)
+        {
+            throw new JsonException($"Expected PropertyName token == '{propertyName}'");
+        }
+
+        reader.Read();
+        return reader.GetDouble();
+    }
+
+    public static int GetInt32(this ref Utf8JsonReader reader, string propertyName)
+    {
+        if (reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new JsonException(
+                $"Expected PropertyName token ('{propertyName}'), but got {reader.TokenType}"
+            );
+        }
+
+        var propName = reader.GetString();
+        if (propName != propertyName)
+        {
+            throw new JsonException($"Expected PropertyName token == '{propertyName}'");
+        }
+
+        reader.Read();
+        return reader.GetInt32();
+    }
+
+    public static uint GetUInt32(this ref Utf8JsonReader reader, string propertyName)
+    {
+        if (reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new JsonException(
+                $"Expected PropertyName token ('{propertyName}'), but got {reader.TokenType}"
+            );
+        }
+
+        var propName = reader.GetString();
+        if (propName != propertyName)
+        {
+            throw new JsonException($"Expected PropertyName token == '{propertyName}'");
+        }
+
+        reader.Read();
+        return reader.GetUInt32();
+    }
+
+    public static bool GetBoolean(this ref Utf8JsonReader reader, string propertyName)
+    {
+        if (reader.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new JsonException(
+                $"Expected PropertyName token ('{propertyName}'), but got {reader.TokenType}"
+            );
+        }
+
+        var propName = reader.GetString();
+        if (propName != propertyName)
+        {
+            throw new JsonException($"Expected PropertyName token == '{propertyName}'");
+        }
+
+        reader.Read();
+        return reader.GetBoolean();
+    }
+}
