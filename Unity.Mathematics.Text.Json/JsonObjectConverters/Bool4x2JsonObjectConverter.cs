c4760c07962497bb5334dbc161861cff81508cc1
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:17 +0900

    refactor: object-based JsonConverter for bool4x2 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Bool4x2JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Bool4x2JsonObjectConverter.cs
@@ -6,74 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Bool4x2JsonObjectConverter : JsonConverter<bool4x2>
+public class Bool4x2JsonObjectConverter : Bool4x2JsonConverter
 {
-    public override bool4x2 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new bool4x2();
-
-        reader.Read();
-        value.c0.x = reader.GetBoolean("m00");
-
-        reader.Read();
-        value.c0.y = reader.GetBoolean("m01");
-
-        reader.Read();
-        value.c0.z = reader.GetBoolean("m02");
-
-        reader.Read();
-        value.c0.w = reader.GetBoolean("m03");
-
-        reader.Read();
-        value.c1.x = reader.GetBoolean("m10");
-
-        reader.Read();
-        value.c1.y = reader.GetBoolean("m11");
-
-        reader.Read();
-        value.c1.z = reader.GetBoolean("m12");
-
-        reader.Read();
-        value.c1.w = reader.GetBoolean("m13");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, bool4x2 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteBoolean("m00", value.c0.x);
-
-        writer.WriteBoolean("m01", value.c0.y);
-
-        writer.WriteBoolean("m02", value.c0.z);
-
-        writer.WriteBoolean("m03", value.c0.w);
-
-        writer.WriteBoolean("m10", value.c1.x);
-
-        writer.WriteBoolean("m11", value.c1.y);
-
-        writer.WriteBoolean("m12", value.c1.z);
-
-        writer.WriteBoolean("m13", value.c1.w);
-
-        writer.WriteEndObject();
-    }
+    public Bool4x2JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
