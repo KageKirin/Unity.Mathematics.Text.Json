8b51ce25857fd5638dc132c53de53cd3b2c9d136
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:09 +0900

    feat: implement object notation-only JsonConverter class for int4

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/JsonConverters/Int4JsonConverter.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public class Int4JsonConverter : JsonConverter<int4>
+{
+    public override int4 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new int4();
+
+        reader.Read();
+        value.x = reader.GetInt32("x");
+
+        reader.Read();
+        value.y = reader.GetInt32("y");
+
+        reader.Read();
+        value.z = reader.GetInt32("z");
+
+        reader.Read();
+        value.w = reader.GetInt32("w");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, int4 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("x", value.x);
+
+        writer.WriteNumber("y", value.y);
+
+        writer.WriteNumber("z", value.z);
+
+        writer.WriteNumber("w", value.w);
+
+        writer.WriteEndObject();
+    }
+}
