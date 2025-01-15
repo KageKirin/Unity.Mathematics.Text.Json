d46885c104f495889ce3e687e5bf12e9c55e4654
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:15 +0900

    refactor: object-based JsonConverter for int4x2 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Int4x2JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Int4x2JsonObjectConverter.cs
@@ -6,74 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Int4x2JsonObjectConverter : JsonConverter<int4x2>
+public class Int4x2JsonObjectConverter : Int4x2JsonConverter
 {
-    public override int4x2 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new int4x2();
-
-        reader.Read();
-        value.c0.x = reader.GetInt32("m00");
-
-        reader.Read();
-        value.c0.y = reader.GetInt32("m01");
-
-        reader.Read();
-        value.c0.z = reader.GetInt32("m02");
-
-        reader.Read();
-        value.c0.w = reader.GetInt32("m03");
-
-        reader.Read();
-        value.c1.x = reader.GetInt32("m10");
-
-        reader.Read();
-        value.c1.y = reader.GetInt32("m11");
-
-        reader.Read();
-        value.c1.z = reader.GetInt32("m12");
-
-        reader.Read();
-        value.c1.w = reader.GetInt32("m13");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, int4x2 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteNumber("m00", value.c0.x);
-
-        writer.WriteNumber("m01", value.c0.y);
-
-        writer.WriteNumber("m02", value.c0.z);
-
-        writer.WriteNumber("m03", value.c0.w);
-
-        writer.WriteNumber("m10", value.c1.x);
-
-        writer.WriteNumber("m11", value.c1.y);
-
-        writer.WriteNumber("m12", value.c1.z);
-
-        writer.WriteNumber("m13", value.c1.w);
-
-        writer.WriteEndObject();
-    }
+    public Int4x2JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
