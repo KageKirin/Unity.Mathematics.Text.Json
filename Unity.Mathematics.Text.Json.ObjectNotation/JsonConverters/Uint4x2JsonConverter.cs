d4c53a639e771a442d5f4b4eea8d071e7d997bef
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:10 +0900

    feat: implement object notation-only JsonConverter class for uint4x2

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/JsonConverters/Uint4x2JsonConverter.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public class Uint4x2JsonConverter : JsonConverter<uint4x2>
+{
+    public override uint4x2 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new uint4x2();
+
+        reader.Read();
+        value.c0.x = reader.GetUInt32("m00");
+
+        reader.Read();
+        value.c0.y = reader.GetUInt32("m01");
+
+        reader.Read();
+        value.c0.z = reader.GetUInt32("m02");
+
+        reader.Read();
+        value.c0.w = reader.GetUInt32("m03");
+
+        reader.Read();
+        value.c1.x = reader.GetUInt32("m10");
+
+        reader.Read();
+        value.c1.y = reader.GetUInt32("m11");
+
+        reader.Read();
+        value.c1.z = reader.GetUInt32("m12");
+
+        reader.Read();
+        value.c1.w = reader.GetUInt32("m13");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, uint4x2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("m00", value.c0.x);
+
+        writer.WriteNumber("m01", value.c0.y);
+
+        writer.WriteNumber("m02", value.c0.z);
+
+        writer.WriteNumber("m03", value.c0.w);
+
+        writer.WriteNumber("m10", value.c1.x);
+
+        writer.WriteNumber("m11", value.c1.y);
+
+        writer.WriteNumber("m12", value.c1.z);
+
+        writer.WriteNumber("m13", value.c1.w);
+
+        writer.WriteEndObject();
+    }
+}
