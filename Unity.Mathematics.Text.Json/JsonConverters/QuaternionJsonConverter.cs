e8a021a670ff8fb201757dabe4c4fead9b75d34f
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:29:04 +0900

    feat: implement common JsonConverter class for quaternion

--- Unity.Mathematics.Text.Json/JsonConverters/QuaternionJsonConverter.cs
+++ Unity.Mathematics.Text.Json/JsonConverters/QuaternionJsonConverter.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -7,24 +8,15 @@ using Unity.Mathematics;
 namespace Unity.Mathematics.Text.Json;
 
 public abstract class QuaternionJsonConverter : JsonConverter<quaternion>
+
 {
-    public delegate quaternion ReadFunc(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    );
 
-    public delegate void WriteFunc(
-        Utf8JsonWriter writer,
-        quaternion value,
-        JsonSerializerOptions options
-    );
+    public delegate quaternion ReadFunc(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options);
 
-    public quaternion ReadAsArray(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
+    public delegate void WriteFunc(Utf8JsonWriter writer, quaternion value, JsonSerializerOptions options);
+
+    public quaternion ReadAsArray(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
     {
         if (reader.TokenType != JsonTokenType.StartArray)
         {
@@ -32,18 +24,19 @@ public abstract class QuaternionJsonConverter : JsonConverter<quaternion>
         }
 
         var value = new quaternion();
-
+        
         reader.Read();
         value.value.x = (float)reader.GetDouble();
-
+        
         reader.Read();
         value.value.y = (float)reader.GetDouble();
-
+        
         reader.Read();
         value.value.z = (float)reader.GetDouble();
-
+        
         reader.Read();
         value.value.w = (float)reader.GetDouble();
+        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndArray)
@@ -55,25 +48,26 @@ public abstract class QuaternionJsonConverter : JsonConverter<quaternion>
     }
 
     public void WriteAsArray(Utf8JsonWriter writer, quaternion value, JsonSerializerOptions options)
+
     {
+
         writer.WriteStartArray();
-
+        
         writer.WriteNumberValue(value.value.x);
-
+        
         writer.WriteNumberValue(value.value.y);
-
+        
         writer.WriteNumberValue(value.value.z);
-
+        
         writer.WriteNumberValue(value.value.w);
-
+        
         writer.WriteEndArray();
+
     }
 
-    public quaternion ReadAsObject(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
+
+    public quaternion ReadAsObject(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
     {
         if (reader.TokenType != JsonTokenType.StartObject)
         {
@@ -81,18 +75,19 @@ public abstract class QuaternionJsonConverter : JsonConverter<quaternion>
         }
 
         var value = new quaternion();
-
+        
         reader.Read();
         value.value.x = (float)reader.GetDouble("x");
-
+        
         reader.Read();
         value.value.y = (float)reader.GetDouble("y");
-
+        
         reader.Read();
         value.value.z = (float)reader.GetDouble("z");
-
+        
         reader.Read();
         value.value.w = (float)reader.GetDouble("w");
+        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndObject)
@@ -103,70 +98,62 @@ public abstract class QuaternionJsonConverter : JsonConverter<quaternion>
         return value;
     }
 
-    public void WriteAsObject(
-        Utf8JsonWriter writer,
-        quaternion value,
-        JsonSerializerOptions options
-    )
+    public void WriteAsObject(Utf8JsonWriter writer, quaternion value, JsonSerializerOptions options)
+
     {
+
         writer.WriteStartObject();
-
+        
         writer.WriteNumber("x", value.value.x);
-
+        
         writer.WriteNumber("y", value.value.y);
-
+        
         writer.WriteNumber("z", value.value.z);
-
+        
         writer.WriteNumber("w", value.value.w);
-
+        
         writer.WriteEndObject();
+
     }
 
-    public quaternion ReadCompatible(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    ) =>
-        reader.TokenType switch
+    public quaternion ReadCompatible(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
+    =>
+         reader.TokenType switch
         {
             JsonTokenType.StartArray => ReadAsArray(ref reader, typeToConvert, options),
             JsonTokenType.StartObject => ReadAsObject(ref reader, typeToConvert, options),
             _ => throw new JsonException(),
         };
+    
 
     private readonly ReadFunc readFunc;
     private readonly WriteFunc writeFunc;
 
-    public QuaternionJsonConverter(
-        JsonTokenType readerTokenType = JsonTokenType.None,
-        JsonTokenType writerTokenType = JsonTokenType.None
-    )
-        : base()
+    public QuaternionJsonConverter(JsonTokenType readerTokenType = JsonTokenType.None, JsonTokenType writerTokenType = JsonTokenType.None) : base()
+
     {
-        readFunc = readerTokenType switch
-        {
+
+        readFunc = readerTokenType switch {
             JsonTokenType.StartArray => ReadAsArray,
             JsonTokenType.StartObject => ReadAsObject,
             _ => ReadCompatible,
         };
 
-        writeFunc = writerTokenType switch
-        {
+        writeFunc = writerTokenType switch {
             JsonTokenType.StartArray => WriteAsArray,
             JsonTokenType.StartObject => WriteAsObject,
             _ => WriteAsArray, //!< we need _some_ kind of default
         };
+
     }
 
-    public override quaternion Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    ) => readFunc(ref reader, typeToConvert, options);
 
-    public override void Write(
-        Utf8JsonWriter writer,
-        quaternion value,
-        JsonSerializerOptions options
-    ) => writeFunc(writer, value, options);
+    public override quaternion Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => readFunc(ref reader, typeToConvert, options);
+
+    public override void Write(Utf8JsonWriter writer, quaternion value, JsonSerializerOptions options)
+        => writeFunc(writer, value, options);
+
 }
+
