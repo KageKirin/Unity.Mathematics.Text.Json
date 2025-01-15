d8326853df9f40121ab247938aeaef26f4795a95
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:29:04 +0900

    feat: implement common JsonConverter class for float4

--- Unity.Mathematics.Text.Json/JsonConverters/Float4JsonConverter.cs
+++ Unity.Mathematics.Text.Json/JsonConverters/Float4JsonConverter.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -7,24 +8,15 @@ using Unity.Mathematics;
 namespace Unity.Mathematics.Text.Json;
 
 public abstract class Float4JsonConverter : JsonConverter<float4>
+
 {
-    public delegate float4 ReadFunc(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    );
 
-    public delegate void WriteFunc(
-        Utf8JsonWriter writer,
-        float4 value,
-        JsonSerializerOptions options
-    );
+    public delegate float4 ReadFunc(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options);
 
-    public float4 ReadAsArray(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
+    public delegate void WriteFunc(Utf8JsonWriter writer, float4 value, JsonSerializerOptions options);
+
+    public float4 ReadAsArray(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
     {
         if (reader.TokenType != JsonTokenType.StartArray)
         {
@@ -32,18 +24,19 @@ public abstract class Float4JsonConverter : JsonConverter<float4>
         }
 
         var value = new float4();
-
+        
         reader.Read();
         value.x = (float)reader.GetDouble();
-
+        
         reader.Read();
         value.y = (float)reader.GetDouble();
-
+        
         reader.Read();
         value.z = (float)reader.GetDouble();
-
+        
         reader.Read();
         value.w = (float)reader.GetDouble();
+        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndArray)
@@ -55,25 +48,26 @@ public abstract class Float4JsonConverter : JsonConverter<float4>
     }
 
     public void WriteAsArray(Utf8JsonWriter writer, float4 value, JsonSerializerOptions options)
+
     {
+
         writer.WriteStartArray();
-
+        
         writer.WriteNumberValue(value.x);
-
+        
         writer.WriteNumberValue(value.y);
-
+        
         writer.WriteNumberValue(value.z);
-
+        
         writer.WriteNumberValue(value.w);
-
+        
         writer.WriteEndArray();
+
     }
 
-    public float4 ReadAsObject(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
+
+    public float4 ReadAsObject(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
     {
         if (reader.TokenType != JsonTokenType.StartObject)
         {
@@ -81,18 +75,19 @@ public abstract class Float4JsonConverter : JsonConverter<float4>
         }
 
         var value = new float4();
-
+        
         reader.Read();
         value.x = (float)reader.GetDouble("x");
-
+        
         reader.Read();
         value.y = (float)reader.GetDouble("y");
-
+        
         reader.Read();
         value.z = (float)reader.GetDouble("z");
-
+        
         reader.Read();
         value.w = (float)reader.GetDouble("w");
+        
 
         reader.Read();
         if (reader.TokenType != JsonTokenType.EndObject)
@@ -104,65 +99,61 @@ public abstract class Float4JsonConverter : JsonConverter<float4>
     }
 
     public void WriteAsObject(Utf8JsonWriter writer, float4 value, JsonSerializerOptions options)
+
     {
+
         writer.WriteStartObject();
-
+        
         writer.WriteNumber("x", value.x);
-
+        
         writer.WriteNumber("y", value.y);
-
+        
         writer.WriteNumber("z", value.z);
-
+        
         writer.WriteNumber("w", value.w);
-
+        
         writer.WriteEndObject();
+
     }
 
-    public float4 ReadCompatible(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    ) =>
-        reader.TokenType switch
+    public float4 ReadCompatible(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
+    =>
+         reader.TokenType switch
         {
             JsonTokenType.StartArray => ReadAsArray(ref reader, typeToConvert, options),
             JsonTokenType.StartObject => ReadAsObject(ref reader, typeToConvert, options),
             _ => throw new JsonException(),
         };
+    
 
     private readonly ReadFunc readFunc;
     private readonly WriteFunc writeFunc;
 
-    public Float4JsonConverter(
-        JsonTokenType readerTokenType = JsonTokenType.None,
-        JsonTokenType writerTokenType = JsonTokenType.None
-    )
-        : base()
+    public Float4JsonConverter(JsonTokenType readerTokenType = JsonTokenType.None, JsonTokenType writerTokenType = JsonTokenType.None) : base()
+
     {
-        readFunc = readerTokenType switch
-        {
+
+        readFunc = readerTokenType switch {
             JsonTokenType.StartArray => ReadAsArray,
             JsonTokenType.StartObject => ReadAsObject,
             _ => ReadCompatible,
         };
 
-        writeFunc = writerTokenType switch
-        {
+        writeFunc = writerTokenType switch {
             JsonTokenType.StartArray => WriteAsArray,
             JsonTokenType.StartObject => WriteAsObject,
             _ => WriteAsArray, //!< we need _some_ kind of default
         };
+
     }
 
-    public override float4 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    ) => readFunc(ref reader, typeToConvert, options);
 
-    public override void Write(
-        Utf8JsonWriter writer,
-        float4 value,
-        JsonSerializerOptions options
-    ) => writeFunc(writer, value, options);
+    public override float4 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => readFunc(ref reader, typeToConvert, options);
+
+    public override void Write(Utf8JsonWriter writer, float4 value, JsonSerializerOptions options)
+        => writeFunc(writer, value, options);
+
 }
+
