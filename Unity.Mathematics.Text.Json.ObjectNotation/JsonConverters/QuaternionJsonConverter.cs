dff2dfcf704707daadc2cd4cefeabc9086695662
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:08 +0900

    feat: implement object notation-only JsonConverter class for quaternion

---
+++ Unity.Mathematics.Text.Json.ObjectNotation/JsonConverters/QuaternionJsonConverter.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ObjectNotation;
+
+public class QuaternionJsonConverter : JsonConverter<quaternion>
+{
+    public override quaternion Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var value = new quaternion();
+
+        reader.Read();
+        value.value.x = (float)reader.GetDouble("x");
+
+        reader.Read();
+        value.value.y = (float)reader.GetDouble("y");
+
+        reader.Read();
+        value.value.z = (float)reader.GetDouble("z");
+
+        reader.Read();
+        value.value.w = (float)reader.GetDouble("w");
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndObject)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        quaternion value,
+        JsonSerializerOptions options
+    )
+    {
+        writer.WriteStartObject();
+
+        writer.WriteNumber("x", value.value.x);
+
+        writer.WriteNumber("y", value.value.y);
+
+        writer.WriteNumber("z", value.value.z);
+
+        writer.WriteNumber("w", value.value.w);
+
+        writer.WriteEndObject();
+    }
+}
