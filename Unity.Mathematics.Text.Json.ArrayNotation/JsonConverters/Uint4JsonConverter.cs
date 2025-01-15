63ab4f30f4282cf20b3ce1945eafc12aa35096a2
Christian Helmich <kagekirin@gmail.com>
2025-01-15 14:00:06 +0900

    feat: implement array notation-only JsonConverter class for uint4

---
+++ Unity.Mathematics.Text.Json.ArrayNotation/JsonConverters/Uint4JsonConverter.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Unity.Mathematics;
+
+namespace Unity.Mathematics.Text.Json.ArrayNotation;
+
+public class Uint4JsonConverter : JsonConverter<uint4>
+{
+    public override uint4 Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException();
+        }
+
+        var value = new uint4();
+
+        reader.Read();
+        value.x = reader.GetUInt32();
+
+        reader.Read();
+        value.y = reader.GetUInt32();
+
+        reader.Read();
+        value.z = reader.GetUInt32();
+
+        reader.Read();
+        value.w = reader.GetUInt32();
+
+        reader.Read();
+        if (reader.TokenType != JsonTokenType.EndArray)
+        {
+            throw new JsonException();
+        }
+
+        return value;
+    }
+
+    public override void Write(Utf8JsonWriter writer, uint4 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        writer.WriteNumberValue(value.x);
+
+        writer.WriteNumberValue(value.y);
+
+        writer.WriteNumberValue(value.z);
+
+        writer.WriteNumberValue(value.w);
+
+        writer.WriteEndArray();
+    }
+}
