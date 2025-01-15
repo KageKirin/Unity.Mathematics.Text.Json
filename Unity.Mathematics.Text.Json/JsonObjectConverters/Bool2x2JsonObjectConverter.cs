354412da568c9d7e4f7bc806afa6419c02e40e8c
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:17 +0900

    refactor: object-based JsonConverter for bool2x2 inherits from common class

--- Unity.Mathematics.Text.Json/JsonObjectConverters/Bool2x2JsonObjectConverter.cs
+++ Unity.Mathematics.Text.Json/JsonObjectConverters/Bool2x2JsonObjectConverter.cs
@@ -6,54 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Bool2x2JsonObjectConverter : JsonConverter<bool2x2>
+public class Bool2x2JsonObjectConverter : Bool2x2JsonConverter
 {
-    public override bool2x2 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartObject)
-        {
-            throw new JsonException();
-        }
-
-        var value = new bool2x2();
-
-        reader.Read();
-        value.c0.x = reader.GetBoolean("m00");
-
-        reader.Read();
-        value.c0.y = reader.GetBoolean("m01");
-
-        reader.Read();
-        value.c1.x = reader.GetBoolean("m10");
-
-        reader.Read();
-        value.c1.y = reader.GetBoolean("m11");
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndObject)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(Utf8JsonWriter writer, bool2x2 value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        writer.WriteBoolean("m00", value.c0.x);
-
-        writer.WriteBoolean("m01", value.c0.y);
-
-        writer.WriteBoolean("m10", value.c1.x);
-
-        writer.WriteBoolean("m11", value.c1.y);
-
-        writer.WriteEndObject();
-    }
+    public Bool2x2JsonObjectConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartObject //!< write as object
+        ) { }
 }
