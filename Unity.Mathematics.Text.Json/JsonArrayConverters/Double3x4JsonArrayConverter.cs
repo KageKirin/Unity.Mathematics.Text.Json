81ada624fcd4c84bf4e4076d5274d645f6ff6815
Christian Helmich <kagekirin@gmail.com>
2025-01-15 20:47:12 +0900

    refactor: array-based JsonConverter for double3x4 inherits from common class

--- Unity.Mathematics.Text.Json/JsonArrayConverters/Double3x4JsonArrayConverter.cs
+++ Unity.Mathematics.Text.Json/JsonArrayConverters/Double3x4JsonArrayConverter.cs
@@ -6,98 +6,11 @@ using Unity.Mathematics;
 
 namespace Unity.Mathematics.Text.Json;
 
-public class Double3x4JsonArrayConverter : JsonConverter<double3x4>
+public class Double3x4JsonArrayConverter : Double3x4JsonConverter
 {
-    public override double3x4 Read(
-        ref Utf8JsonReader reader,
-        Type typeToConvert,
-        JsonSerializerOptions options
-    )
-    {
-        if (reader.TokenType != JsonTokenType.StartArray)
-        {
-            throw new JsonException();
-        }
-
-        var value = new double3x4();
-
-        reader.Read();
-        value.c0.x = reader.GetDouble();
-
-        reader.Read();
-        value.c0.y = reader.GetDouble();
-
-        reader.Read();
-        value.c0.z = reader.GetDouble();
-
-        reader.Read();
-        value.c1.x = reader.GetDouble();
-
-        reader.Read();
-        value.c1.y = reader.GetDouble();
-
-        reader.Read();
-        value.c1.z = reader.GetDouble();
-
-        reader.Read();
-        value.c2.x = reader.GetDouble();
-
-        reader.Read();
-        value.c2.y = reader.GetDouble();
-
-        reader.Read();
-        value.c2.z = reader.GetDouble();
-
-        reader.Read();
-        value.c3.x = reader.GetDouble();
-
-        reader.Read();
-        value.c3.y = reader.GetDouble();
-
-        reader.Read();
-        value.c3.z = reader.GetDouble();
-
-        reader.Read();
-        if (reader.TokenType != JsonTokenType.EndArray)
-        {
-            throw new JsonException();
-        }
-
-        return value;
-    }
-
-    public override void Write(
-        Utf8JsonWriter writer,
-        double3x4 value,
-        JsonSerializerOptions options
-    )
-    {
-        writer.WriteStartArray();
-
-        writer.WriteNumberValue(value.c0.x);
-
-        writer.WriteNumberValue(value.c0.y);
-
-        writer.WriteNumberValue(value.c0.z);
-
-        writer.WriteNumberValue(value.c1.x);
-
-        writer.WriteNumberValue(value.c1.y);
-
-        writer.WriteNumberValue(value.c1.z);
-
-        writer.WriteNumberValue(value.c2.x);
-
-        writer.WriteNumberValue(value.c2.y);
-
-        writer.WriteNumberValue(value.c2.z);
-
-        writer.WriteNumberValue(value.c3.x);
-
-        writer.WriteNumberValue(value.c3.y);
-
-        writer.WriteNumberValue(value.c3.z);
-
-        writer.WriteEndArray();
-    }
+    public Double3x4JsonArrayConverter()
+        : base(
+            readerTokenType: JsonTokenType.None, //!< compatible read
+            writerTokenType: JsonTokenType.StartArray //!< write as array
+        ) { }
 }
