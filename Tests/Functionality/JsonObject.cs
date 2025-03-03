fd0ad1f66b850c76888c3539ce44920a4863d353
Christian Helmich <kagekirin@gmail.com>
2025-03-03 13:46:25 +0900

    refactor (Functionality unit tests): remove unnecessary usings from JsonObject.cs

--- Tests/Functionality/JsonObject.cs
+++ Tests/Functionality/JsonObject.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using Unity.Mathematics;
-using Unity.Mathematics.Text.Json;
 using Xunit;
-
-#nullable enable
 
 namespace Unity.Mathematics.Text.Json.JsonObject.Tests;
 
